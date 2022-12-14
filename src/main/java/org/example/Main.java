35e9f9944aab10a7d8a57fa77575903c04861c9d
WIN-JJH83PJTM7J\mkadmin <r.yakupov81@yandex.ru>
2022-12-14 17:31:45 +0300

    начал задачу на ООП

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,6 +9,15 @@ public class Main {
 //    метод интерфейса, для валидного перевода величин. Метод для конвертации
 //    назовите "convertValue".
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите температуру: ");
+        double a = scanner.nextInt();
+        scanner.close();
+        Temperature celsium = new Temperature(a);
+//        double b=22.2;
+//        b = celsium.temperCelsius(a);
+        System.out.println("Temperature Celsius =  " + a);
+        System.out.println("Temperature Kelvin =  " + celsium.tempKelvin(celsium));
+        System.out.println("Temperature Fahrenheit =  " + celsium.tempFahrenheit(celsium));
     }
 }
--- src/main/java/org/example/Temperature.java
+++ src/main/java/org/example/Temperature.java
@@ -1,5 +1,33 @@
 package org.example;
 
 public class Temperature {
+    private Double temperCelsius;
+
+//    public Double getTemperCelsius() {
+//        return temperCelsius;
+//    }
+
+    public Temperature(Double temperCelsius) {
+        this.temperCelsius = temperCelsius;
+    }
+
+//    @Override
+//    public String toTemperature(){
+//        return "Temperature Celsius : " + temperCelsius;
+//    }
+
+    /**
+     * @return температура Кельвин
+     */
+    public double tempKelvin(Temperature temperature) {
+        return temperature.temperCelsius + 273.15;
+    }
+
+    /**
+     * @return температура Фаренгейт
+     */
+    public double tempFahrenheit(Temperature temperature) {
+        return ((9 / 5) * temperature.temperCelsius) + 32;
+    }
 
 }
